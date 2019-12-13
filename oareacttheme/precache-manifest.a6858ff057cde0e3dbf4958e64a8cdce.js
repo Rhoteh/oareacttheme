aae0dfe3eaab276b3c6898479728b6b8aa8b1089
Rotimi <Rhoteh@gmail.com>
2019-12-13 17:59:14 +0200

    Add files via upload

---
+++ oareacttheme/precache-manifest.a6858ff057cde0e3dbf4958e64a8cdce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6ebbb58a69dd1c332861af6838cf4c",
+    "url": "/wp-content/themes/oareacttheme/index.php"
+  },
+  {
+    "revision": "d1aa2cc9fec85890eb6f",
+    "url": "/wp-content/themes/oareacttheme/static/css/main.chunk.css"
+  },
+  {
+    "revision": "dee4ee419c0c75760255",
+    "url": "/wp-content/themes/oareacttheme/static/js/2.chunk.js"
+  },
+  {
+    "revision": "e1db60ac374ff8be07e7",
+    "url": "/wp-content/themes/oareacttheme/static/js/bundle.js"
+  },
+  {
+    "revision": "d1aa2cc9fec85890eb6f",
+    "url": "/wp-content/themes/oareacttheme/static/js/main.chunk.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/wp-content/themes/oareacttheme/static/media/logo.25bf045c.svg"
+  }
+]);
